8824d08630a7a61c7d9d106ed4240456ed94d974
favas-cv <fawazmuhd06@example.com>
2026-07-02 14:02:53 +0530

    fix api login ,

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000/api/'; //import.meta.env.VITE_API_URL || 
+const API_URL = import.meta.env.VITE_API_URL; //||'http://127.0.0.1:8000/api/';  
 console.log("API URL:", API_URL);
 const api = axios.create({
   baseURL: API_URL,
